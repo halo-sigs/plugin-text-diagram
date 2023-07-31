d5ce914b0af12e7a35a74464bc9735f8e27424a7
gengxin <1376732967@qq.com>
2023-07-31 11:28:45 +0800

    fix: Remove redundant CSS import in JSInjector.

--- src/main/java/run/halo/plugin/textdiagram/JSInjector.java
+++ src/main/java/run/halo/plugin/textdiagram/JSInjector.java
@@ -3,7 +3,6 @@ package run.halo.plugin.textdiagram;
 public class JSInjector {
     static String getParsedKatexScript(String mermaid_selector) {
         String katexScript = """
-            <link rel="stylesheet" href="/plugins/plugin-katex/assets/static/katex.min.css">
             <script defer src="/plugins/plugin-text-diagram/assets/static/mermaid.min.js"></script>
             <script>
                 document.addEventListener("DOMContentLoaded", function() {
