177e263a6908ee8cc28ce95f719c5476f682938f
XinKeng <29268441+gengxiaoxiaoxin@users.noreply.github.com>
2024-01-15 15:21:07 +0800

    fix: fix vue typings ts-loader

--- console/env.d.ts
+++ console/env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
 
 declare module "*.vue" {
-  import Vue from "vue";
-  export default Vue;
+  declare module "*.vue" {
+    // NOTE: ts-loader
+    import { defineComponent } from "vue";
+
+    const component: ReturnType<typeof defineComponent>;
+    export default component;
+  }
 }
